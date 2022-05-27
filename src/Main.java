353d534b7ce3e5db351a92508fc04c8f519924b0
Quinn Van Order <42633398+quinnvanorder@users.noreply.github.com>
2022-05-27 14:26:18 -0700

    Created framework and detailed challenge in main.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+Write a method isLeapYear with a parameter of type int named year.
+THe parameter needs to be greater than or equal to 1 and less than or equal to 9999
+If the parameter is not in that range return false
+Otherwise if it is in the valid range calculate if the year is a leap year and return true if it is, otherwise return false.
+A year is a leap year if it is divisible by 4, but not by 100, or if it is divisible by 400.
+
+Write another method getDaysInMonth with two parameters month and year, both type int.
+if parameter month is < 1 or > 12 return -1
+if parameter year is <1 or > 9999 return -1
+The method needs to return the number of days in the month, be careful about leap years as they have 29 days in month 2.
+
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
